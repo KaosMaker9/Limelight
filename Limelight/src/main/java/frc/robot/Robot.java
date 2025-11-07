fb11b94abc6bf1c04914667fa64008463ef31c04
NotMaxwell <william.max.mccormick@gmail.com>
2025-11-06 20:54:28 -0600

    I tried to copy code from some other guy. I will probabaly roll this back and do it myself.

--- Limelight/src/main/java/frc/robot/Robot.java
+++ Limelight/src/main/java/frc/robot/Robot.java
@@ -7,8 +7,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.LimelightHelpers;
 import frc.robot.Constants;
+import frc.robot.subsystems.limelight.LimelightHelpers;
+
 import static frc.robot.Constants.validIDs;
 
 public class Robot extends TimedRobot {
